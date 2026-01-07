47bcb26fe9e1386b9bb910e779f652b9e89f04e4
Nam Admin <148678363+TranTrungNamn@users.noreply.github.com>
2026-01-08 02:33:56 +0700

    new public api

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,5 @@
 // app/config.ts
 
-// Chỉ lấy từ biến môi trường, không dùng localhost nữa
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+// Nếu không có biến môi trường, dùng chuỗi rỗng để code không bị crash,
+// nhưng EnvChecker sẽ hiện thông báo lỗi đỏ cho bạn biết.
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
