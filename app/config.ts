f7432291bce8fa82a512664a5f6e45f9ef3075b3
Nam Admin <148678363+TranTrungNamn@users.noreply.github.com>
2026-01-08 02:35:52 +0700

    new public api

--- app/config.ts
+++ app/config.ts
@@ -2,4 +2,4 @@
 
 // Nếu không có biến môi trường, dùng chuỗi rỗng để code không bị crash,
 // nhưng EnvChecker sẽ hiện thông báo lỗi đỏ cho bạn biết.
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
+export const API_URL = "https://beta-want-fed-naturals.trycloudflare.com/";
